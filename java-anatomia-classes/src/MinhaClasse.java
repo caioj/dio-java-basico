3e067a8766a37d8775b2d383a82fed5f6d22a68c
Caio <63052612+caioj@users.noreply.github.com>
2023-01-10 18:12:18 -0300

    Add files via upload

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    //CORPO DA CLASSE
+public static void main (String [] args) {
+    System.out.print("Olá pessoal, sejam bem-vindos");
+    //CORPO DO MÉTODO main
+}
+
+}
